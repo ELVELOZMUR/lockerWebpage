da9335527f153c172f871be498ca3cb9397fb83a
Veloz <elvelozmur6@gmail.com>
2026-08-11 00:09:32 -0300

    help, i am trying to change the background color

--- main.js
+++ main.js
@@ -239,7 +239,7 @@ Main.main = function() {
 	sources.style.justifyContent = "center";
 	sources.style.alignItems = "start";
 	sources.style.gap = "20px";
-	sources.style.backgroundColor = "#3B3030";
+	sources.style.backgroundColor = "#858585";
 	sources.style.backgroundImage = "radial-gradient(#6E6E6E 5px, transparent 2px)";
 	sources.style.backgroundSize = "60px 5px";
 	window.document.body.appendChild(sources);
